941116b451d68358c2b61a328627007377194422
Carina <carrie.poell@gmail.com>
2019-05-17 17:29:43 +0200

    Restructured HTML

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -1,13 +1,7 @@
 
 $(document).ready(function(){
 
-    
-    $("#jumbo").on("mouseenter", function() {
-        $(this).removeClass("jumbotron-style").addClass("alt-jumbotron");
-        });
-        
-    $("#jumbo").on("mouseleave", function() {
-        $(this).removeClass("alt-jumbotron").addClass("jumbotron-style");
-        });
+
+    $("#winter-btn").addClass("flicker-in-1");
         
 });
