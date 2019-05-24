6627e58df7574d79857758b54567503a169ff6ff
Carina <carrie.poell@gmail.com>
2019-05-24 21:20:54 +0200

    Fix first graph; work on second

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -6,27 +6,60 @@ function makeGraphs(error, gotData) {
     var ndx = crossfilter(gotData);
     
     
-    show_viewership_by_season(ndx);
+    show_total_viewership_by_season(ndx);
+    // show_avg_viewership_by_season(ndx);
     
     dc.renderAll();
 }
 
+	// reuseable custom reduce average function 
+	function reduceAvg(dimension, type) {
+		return dimension.groupAll().reduce(
+			function(p, v) {
+				p.count++;
+				p.total += v[type];
+				p.average = p.total / p.count;
+				return p;
+			},
 
-function show_viewership_by_season(ndx) {
+			function(p, v) {
+				p.count--;
+				p.total -= v[type];
+				p.average = p.total / p.count;
+				return p;
+			},
+
+			function() {
+				return {
+					count: 0,
+					total: 0,
+					average: 0
+				};
+			}
+		);
+    }
+    
+
+    // Individual Graph Functions
+
+function show_total_viewership_by_season(ndx) {
 
     var seasonDim = ndx.dimension(dc.pluck('Season'));
-    var viewership_per_season = seasonDim.group().reduceSum(dc.pluck('US viewers (million)'));
+    var total_viewership_per_season = seasonDim.group().reduceSum(dc.pluck('US viewers (million)'));
 
     dc.barChart('#viewsSeason')
         .width(500)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({top: 10, right: 60, bottom: 30, left: 50})
         .dimension(seasonDim)
-        .group(viewership_per_season)
+        .group(total_viewership_per_season)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
+        .y(d3.scale.linear()
+            .domain([0, 85]))
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Season")
         .yAxisLabel("Viewership (in million)")
+        .renderLabel(true)
         .yAxis().ticks(4);
 }
