7833b57c0863d8cb5dc01ff48246f1a12e6683b6
Carina <carrie.poell@gmail.com>
2019-06-11 21:16:01 +0200

    Standardize season coloring

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -417,26 +417,27 @@ function show_deaths_over_time(ndx) {
         .compose([
             dc.lineChart(compositeChart)
             .group(S1Group, 'Season 1')
-            .colors('green'),
+            .colors('#DACC3E'),
             dc.lineChart(compositeChart)
-            .colors('red')
+            .colors('#8A8E91')
             .group(S2Group, 'Season 2'),
             dc.lineChart(compositeChart)
-            .colors('blue')
+            .colors('#EEFFDB')
             .group(S3Group, 'Season 3'),
             dc.lineChart(compositeChart)
-            .colors('black')
+            .colors('#855A5C')
             .group(S4Group, 'Season 4'),
             dc.lineChart(compositeChart)
-            .colors('indigo')
+            .colors('#7FB7BE')
             .group(S5Group, 'Season 5'),
             dc.lineChart(compositeChart)
-            .colors('orange')
+            .colors('#6E403A')
             .group(S6Group, 'Season 6'),
             dc.lineChart(compositeChart)
-            .colors('grey')
+            .colors('#705D56')
             .group(S7Group, 'Season 7'),
             dc.lineChart(compositeChart)
+            .colors('#020202')
             .group(S8Group, 'Season 8'),
         ])
         .brushOn(true)
@@ -450,7 +451,7 @@ function show_percentage_of_deaths_per_season(ndx) {
     var num_death_group = seasonDim.group().reduceSum(dc.pluck('deaths'));
 
     var seasonColors = d3.scale.ordinal()
-        .range(['#6c6cff', 'blue', 'red', 'black', 'indigo', 'orange', 'grey', '#172A3A']);
+        .range(['#6E403A ', '#855A5C', '#8A8E91', '#DACC3E', '#7FB7BE', '#EEFFDB', '#020202', '#705D56']);
 
     dc.pieChart("#deathPercentage")
         .height(300)
@@ -722,7 +723,7 @@ function show_score_by_writer(ndx) {
         .y(d3.scale.linear()
             .domain([5, 10]))
         .xUnits(dc.units.ordinal)
-        .xAxisLabel("Season")
+        .xAxisLabel("Writer")
         .yAxisLabel("IMDB Rating")
         .renderlet(function (chart) {
             chart.selectAll("g.x text")
@@ -750,8 +751,8 @@ function show_correlation_between_rating_and_viewership(ndx) {
 dc.scatterPlot("#corrIMDBandViews")
     .dimension(IMDBViewDim)
     .group(IMDBViewGroup)
-    .width(768)
-    .height(480)
+    .width(700)
+    .height(400)
     .y(d3.scale.linear()
     .domain([0,10]))
     .x(d3.scale.linear()
